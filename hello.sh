946fae97265b5c70b58817e489ac6b738cfcb725
varun3536 <65685408+varun3536@users.noreply.github.com>
2020-11-18 09:48:00 -0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a comment!
-stdbuf --output=0
+stdbuf -o0
 set -o allexport
-ssh -tt -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash ./hello.sh 
-echo "Hello      World"       # This is a comment, too!
-echo "Hello World"
-echo "Hello * World"
+out=$(ssh -tt -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash ./hello.sh << 'EOF'
+
+EOF
+)
+
 
 
 
