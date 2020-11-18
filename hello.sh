119bd41284096cef192c4e582565cb93f6deb4a1
varun3536 <65685408+varun3536@users.noreply.github.com>
2020-11-18 09:55:51 -0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # This is a comment!
 sys.stdout.flush()
-
-ssh -tt -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash ./hello.sh
+ssh -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash ./hello.sh
 
 
 
