1728116d8c78cf84999105e96af561036b91e0e3
varun3536 <65685408+varun3536@users.noreply.github.com>
2020-11-18 11:23:39 -0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a comment!
 
-out=  script -q -c ssh -t -t -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash  ./hello.sh 
+out=  script -q -c "ssh -t -t  -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash  ./hello.sh"
 echo "print"
 echo "${out}"
 
