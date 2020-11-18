b8d18e58248b8403e50c813c9df648f188fa6614
varun3536 <65685408+varun3536@users.noreply.github.com>
2020-11-18 09:51:49 -0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -2,10 +2,8 @@
 # This is a comment!
 sys.stdout.flush()
 set -o allexport
-out=$(ssh -tt -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash ./hello.sh << 'EOF'
+out=$(ssh -tt -o StrictHostKeyChecking=no -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash ./hello.sh)
 
-EOF
-)
 echo "${out}"
 
 
