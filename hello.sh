4937824a0a8d0cf7678e43dd8450cc9516d74f68
varun3536 <65685408+varun3536@users.noreply.github.com>
2020-11-18 23:25:53 -0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a comment!
 set -o allexport
-stdbuf -oL
+
 ssh -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com script -c  ./world.sh | tee test.log
 
 
