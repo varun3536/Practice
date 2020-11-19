b4478a3963d6e43d0c7d6594fd12dbb4b442842c
varun3536 <65685408+varun3536@users.noreply.github.com>
2020-11-18 21:09:52 -0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a comment!
-
-ssh -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash -c stdbuf -oL ./hello.sh 
+stdbuf -oL
+ssh -i /var/lib/jenkins/workspace/mykey ec2-user@ec2-3-89-26-76.compute-1.amazonaws.com /bin/bash -c  ./hello.sh 
 
 
 
